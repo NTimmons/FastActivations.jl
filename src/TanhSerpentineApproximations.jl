fec3118842e88bd43ef1e74441c44943a689f0d9
Páll Haraldsson <Pall.Haraldsson@gmail.com>
2022-10-26 12:04:43 +0000

    Should be faster

--- src/TanhSerpentineApproximations.jl
+++ src/TanhSerpentineApproximations.jl
@@ -1,21 +1,13 @@
-
-
 function Serpentine(x)
-    a =  oftype(x/1,2.0)
-    b =  oftype(x/1,2.0)
-    ( (a*a *x) / (a * b + x*x) )
+    a = oftype(x/1, 2.0)
+    b = oftype(x/1, 2.0)
+    (a*a*x) / (a*b + x*x)
 end
 
 function SerpentineClamp(x)
-    if( x < oftype(x/1,-2.0) )
-        return oftype(x/1, -1.0)
-    elseif( x > oftype(x/1,2.0) )
-        return oftype(x/1, 1.0)
+    if abs(x) < oftype(x/1, 2.0)
+        return Serpentine(x)
     end
-    
-    a =  oftype(x/1,2.0)
-    b =  oftype(x/1,2.0)
-    return ( (a*a *x) / (a * b + x*x) ) 
 end
 
 TanhSerpentineFunctions = [Serpentine, SerpentineClamp]
